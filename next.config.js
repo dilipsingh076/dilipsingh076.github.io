fd82330c81df2a198e557649cd0d88d5452db84d
Dilip Singh <dilipsinghf@gmail.com>
2025-09-16 08:05:16 +0000

    sss

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/dilipsingh076.github.io',
+  assetPrefix: '/dilipsingh076.github.io/',
   images: {
     domains: ['media.istockphoto.com', 'github.com'],
     formats: ['image/webp', 'image/avif'],
